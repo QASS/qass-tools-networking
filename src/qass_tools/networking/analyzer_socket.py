47cade16f43fdb0dc72266e121c64310acc29841
Opti Mizer <opti@opti000000.qass.office>
2022-05-31 12:13:59 +0200

    add all docstrings

--- src/qass_tools/networking/analyzer_socket.py
+++ src/qass_tools/networking/analyzer_socket.py
@@ -50,19 +50,20 @@ class AnalyzerCmd():
         self.s.connect((self.ip, self.port))
 
     def start_measuring(self):
-        """ This method creates a dictionary "command" which is filled with information to start a new measuring. Afterwards the dictionary will be send to the conencted analyzer by the send method 
-        and the command gets executed. The key 'cmd' is signalling that this message is an App Command and over 'msgid' you can assign every send message to an ID. Key "p1" always include the actual command phrase
-        for the analyzer. This commands are implied in the analyzer source code. """
+        """Method sends a command to the connected analyzer to start a maesuring process.
+        """
         command = {'cmd': "AppCmd", "msgid": self.msgid, "p1": "startMeasuring"}
         self._send(command)
 
     def start_sineGenerator(self, frequency: int, amplitude: int):
-        """ This method creates a dictionary "command" which is filled with information to start the sine genrator. It includes the amplitude and frequency settings of the future desired measurement. 
-        Afterwards the dictionary will be send to the conencted analyzer by the send method and the command gets executed. The key 'cmd' is signalling that this message is an App Command and over 
-        'msgid' you can assign every send message to an ID. The key "p1" always include the actual command for the analyzer. Over the last keyword (p2) the desired settings have to be entered as a float. 
-        The variables will be entered as arguments of the function. The first arguments is the deisred frequency while the second one is the desired amplitude. As a delimiter a space letter should be used. Before the
-        command gets sended, input values get checked. If entered amplitude value is saved in enum class, the command will be sended. If not, and ValueError is raisen. The user will be called to enter new settings for
-        sine generator. The function is called again with new settings. Due to that syntax it is secured that even the new settings will be tested."""
+        """Method sends command that sine generator generates a sine wave with custom frequency and amplitude settings.
+
+        :param frequency: Used frequency to generate sine wave.
+        :type frequency: int
+        :param amplitude: Used amplitude to generate sine wave.
+        :type amplitude: int
+        :raises ValueError: Set amplitude has to be equal to one class constances of class Amplitudes. If exception is raised the user is asked to enter new amplitude and frequency. 
+        """
         a = list(Amplitudes)
         try:
             if amplitude in Amplitudes:
@@ -78,54 +79,89 @@ class AnalyzerCmd():
     
 
     def stop_sineGenerator(self):
-        """ This method creates a dictionary "command" which is filled with information to stop the running sine generator. Afterwards the dictionary will be send to the conencted analyzer by the send method
-        and the command gets executed. The key 'cmd' is signalling that this message is an App Command and over 'msgid' you can assign every send message. The key "p1" always include the actual command for 
-        the analyzer. This commands are implied in the analyzer source code. """        
+        """Command to stop generating sine waves.
+        """
         command = {'cmd': "AppCmd", "msgid": self.msgid, "p1": "StopSineGen"}
         self._send(command)
 
     def stop_measuring(self):
-        """ This method creates a dictionary "command" which is filled with information to stop the running measurement. Afterwards the dictionary will be send to the conencted analyzer by the send method
-        and the command gets executed. The key 'cmd' is signalling that this message is an App Command and over 'msgid' you can assign every send message. The key "p1" always include the actual command for 
-        the analyzer. This commands are implied in the analyzer source code. """ 
+        """Command to stop current measuring process.
+        """
         command = {'cmd': "AppCmd", "msgid": self.msgid, "p1": "stopMeasuring"}
         self._send(command)
 
     def set_process_comment(self, proc_comm: str):
-        """ This method creates a dictionary "command" which is filled with information to set a process comment in the current process. Afterwards the dictionary will be send to the conencted analyzer 
-        by the send method and the command gets executed. The created dictionary differs because it has it own ComServer Command. The key 'cmd' now gives information about the analyzer actions and in "p1"
-        are more detailed information saved. In this case this information should be a string. The comment is stored in database under process.comment """
+        """Set a process comment for current selected process.
+
+        Parsed string will be saved in database under entry: process.comment
+
+        :param proc_comm: Text which should be seen and saved as process comment
+        :type proc_comm: str
+        """
         command = {'cmd': "AppCmd", "msgid": self.msgid, "p1": "setprocesscomment", "p2": f"{proc_comm}"}
         self._send(command)
 
     def set_app_var(self, app_var_name:str, app_var_value:any):
-        """ Method to set AppVars in operator network of analyzer. AppVar Operator reads out here set variables. AppVars have to be implemented in operator network first.
-        Command is SetAppVar and due to p1 and p2 details will be set. First key contains kind of AppVar as a string. Name should match with created AppVar in operator network. Key p2 contains the 
-        value of the created variable. As value can be choosed any datatyp supported by python (e.g. float, int, str, json, ...)."""
+        """Parse value to specific AppVar operator in operator network of analyzer.
+
+        There has to be an already existing AppVar operator which can accessed by (matching) name.
+
+        :param app_var_name: Name of existing AppVar operator.
+        :type app_var_name: str
+        :param app_var_value: Value which should be assigned to operator. As value can be choosed any datatyp supported by python (e.g. float, int, str, json, ...).
+        :type app_var_value: any
+        """
         command = {'cmd': "SetAppVar", "msgid": self.msgid, "p1": f"{app_var_name:}", "p2": f"{app_var_value}"}
         self._send(command)    
     
-    def set_app_var_json(self, app_var_name:str, value:any):
-        """ Method to send an AppVar. Placeholder var_name and name of AppVar Reader in operator network have to be equal. An extra method is provided because this method works with an general analyzer AppCommand.
-        Process provided in Method " set_app_vars" uses his own app command. In this method by key "p1" is declared that a new AppVar has to bet set. In "p2" kind of appVar as the correspodning value is set. Name 
-        value is delimited by a space letter. """
+    def set_app_var_appcmd(self, app_var_name:str, value:any):
+        """Parse value to specific AppVar operator in operator network of analyzer.
+
+        An extra method is provided because this method works with an general analyzer AppCommand.
+
+        .. seealso:: set_app_var()
+
+        :param app_var_name: Name of existing AppVar operator.
+        :type app_var_name: str
+        :param app_var_value: Value which should be assigned to operator. As value can be choosed any datatyp supported by python (e.g. float, int, str, json, ...).
+        :type app_var_value: any
+        """
         command = {'cmd': "AppCmd", "msgid": self.msgid, "p1": "SetAppVar", "p2": f"{app_var_name} {value}"}
         self._send(command)
 
-    def get_current_process_number(self) -> int:
+    def get_process_number(self) -> int:
+        """Send command to give out process number as return.
+
+        .. note:: Analyzer response contains more information than only the process number. Private method will extract claimed information.
+        :return: Process number of current selected process
+        :rtype: int
+        """
         command = {'cmd': "getprocessnumber", "msgid": self.msgid}
         return self._send(command)
         
     #TODO: test function
     def create_project(self, project_name:str):
-        """ Method from to create a new Porject with customize name. Parameter is a string with name of the new project. Avoid spaces in name or other typical forbidden characters"""
+        """Create new project after used template with custom name.
+
+        .. note:: Avoid spaces in name or other typical forbidden characters.
+
+        :param project_name: Name of new project
+        :type project_name: str
+        """
         command = {'cmd': "createloadproject", 'msgid': self.msgid, 'p1': project_name}
         self._send(command)
     
     #cmd="getpreampinfo" p1=portnumber
         #TODO: Test function
     def send_AppCmd(self, param_one:str, param_two=None):
-        
+        """General method to send arbitrary AppCmd to analyzer.
+
+        :param param_one: Setting which AppCmd should be used.
+        :type param_one: str
+        :param param_two: If needed second parameter to specify params used in AppCmd, defaults to None
+        :type param_two: str, optional
+        :raises TypeError: Type Check for second parameter, exception is raised if value is not equal to type str.
+        """
         command = {'cmd': "AppCmd", 'msgid': self.msgid, 'p1': param_one}        
         
         if param_two and param_two == str:
@@ -136,14 +172,9 @@ class AnalyzerCmd():
         self._send(command)
 
     def set_preamp(self, user_dict=None, **kwargs):
-        """ Method to set new preamp settings for multiplexer. The default settings can be seen down below. By entering a new value as **kwargs,
-            you are able to change specific values in the default dict, which will be sended. The use of whole new dict is possible to replace all settings with user-defined values.
-            Have in mind that your new dictionary must have identical keys like the default one. Also check supported datatypes and range manually, as a automatic overproof is not provided yet.
-        Args:
-                    None
-        **kwargs:
-                    user_dict (optional, dict): complete new dict with user defined settings
-                    every multiplexer keyword provided in default settings dict
+        """Method to set preamp settings for multiplexer.
+
+        By entering a new value as **kwargs, you are able to change specific values in the default dict, which will be sended. The use of whole new dict is possible to replace all settings with user-defined values. Have in mind that your new dictionary must have identical keys like the default one.
         
         Default settings:
         | Type | Multiplexer     | Value |
@@ -160,7 +191,10 @@ class AnalyzerCmd():
         | bool | filter          | false |
         | int  | gain            | 800   |
         | int  | subport         | 0     |
-        
+
+        .. warning:: Check supported datatypes and range manually, as a automatic overproof is not provided yet.
+        :param user_dict: Possibility to parse your own dictionary instead of editing the default one, defaults to None
+        :type user_dict: Dict, optional
         """
         # helper dict with default values
         default_dict = {'channel': "0",
@@ -200,38 +234,45 @@ class AnalyzerCmd():
     #TODO: new function "CommunicationServer Command zum exportieren des Operatoren Netzes als JSON File"
     
     def get_info(self):
-        """ Method uses communication server command to return a dictionary with analyzer information. Part of the information is the analyzer version as current used project ID and name."""
+        """Method to read out anlyzer informations as current used project ID/name or analyer version.
+
+        :return: Informations out of info window in analyzer.
+        :rtype: Dict
+        """
         command = {'cmd': "getinfo", "msgid": self.msgid}
         return self._send(command)
         
     def _send(self, command: Dict):
-        """ Private method to transform the creates dictioanries in JSON Format and send them to the analyzer. Every sended message got its own unique message ID. After sending the message, the analyzer responses. The
-        response will be read out and decoded after utf-8. For every sended command, the analyzer provides a section in his response which says "true" (command got executed) or "false" (command got not executed)
-        The try statement checks if command get executed and raises an exception if commands were not handled correctly. In case of this exception the analyzer get commands to stop sine generator and measurement.
-        Flag value (set in __init__ method) makes it possible to decide wether commands and their results will be saved in a dictionary. """
-        
+        """_summary_
+
+        _extended_summary_
+
+        :param command: Command which should be send to analyzer
+        :type command: Dict
+        :return : _description_
+        :rtype: _type_
+        """
+        # print every sended command
         print(f"Sended command:\n{command}")
+        # prepare command
         cmd_str = json.dumps(command).encode()
         cmd_str = (len(cmd_str)).to_bytes(2, 'big') + cmd_str
+        # adding msgid 
         self.msgid += 1
+        # actual sending command
         self.s.sendall(cmd_str)
+        
+        # handle special cases reponse
+
+        # setpreamp doesn't send a response at all
         if not "setpreamp" in command['cmd']:
             response = self.s.recv(4096) # readed byte count
             
+            # all AppCmd resopnses have to be decoded by utf-8 and printed out
             if "AppCmd" in command['cmd']:
                 response = response.decode("utf-8") #utf-8 decode type
                 print(f"Optimizer response:\n{response}")
-        # raise exception if message is not send in the right way
-        #     try:
-            #        response = response.decode("utf-8") #utf-8 decode type
-            #        print(response)
-            #        if -1 == response.find("true"):
-            #            raise RuntimeError("Command was not send correctly")
-            #     except RuntimeError: 
-            #        self.stop_sineGenerator()
-            #        self.stop_measuring()
-            #        print("Command was not send correctly")
-            
+        
             elif "getprocessnumber" in command['cmd']:
                 response = response[2:].decode()
                 obj = json.loads(response)
@@ -243,15 +284,9 @@ class AnalyzerCmd():
                 obj = json.loads(response)
                 return obj
 
-        # if (self.flag == True):
-        #     command_status = dict()
-        #     command_status["command"] = command["p1"]
-        #     a = response.rfind('"ok":')
-        #     command_status["status"] = response[(a):(a+5)]
-        #     self._executed_commands.append(command_status)
-
     def close(self):
-        """ Method to close the socket connection between PC and analyzer. """
+        """Method to close the socket connection between machine and analyzer.
+        """
         self.s.close()
 
 
