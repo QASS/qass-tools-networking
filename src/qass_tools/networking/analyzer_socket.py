dfa7aeee2d70d71764e0bf5fb405e36813859ed3
Nicklas Osmers <nosmers@qass.net>
2022-06-03 08:45:56 +0000

    analzyer_socket.py: Fixed spelling in function name "import_project"

--- src/qass_tools/networking/analyzer_socket.py
+++ src/qass_tools/networking/analyzer_socket.py
@@ -204,7 +204,7 @@ class AnalyzerCmd():
         command = {'cmd': "getinfo", "msgid": self.msgid}
         return self._send(command)
     
-    def import_projcect(self, path, project_name, overwrite=False):
+    def import_project(self, path, project_name, overwrite=False):
         """
         Imports a project from a settings export (tar.gz or sqlite export).
         :param path: The (local) path where to find the project settings export.
