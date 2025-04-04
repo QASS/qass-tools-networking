96c0a424d0e749df34ddb50d17984d5d325f592f
Alexander Becker <abecker@qass.office>
2025-04-04 15:34:59 +0200

    Move errors and enums to submodules

---
+++ src/qass/tools/networking/errors.py
@@ -0,0 +1,22 @@
+class ReceiverThreadError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class ConnectionError(Exception):
+    def __init__(self, message):
+        self.message = message 
+
+    def __str__(self):
+        return self.message
+
+
+class AnalyzerError(RuntimeError):
+    def __init__(self, message):
+        self.message = message
+        
+    def __str__(self):
+        return self.message
